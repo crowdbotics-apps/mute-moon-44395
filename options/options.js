6415b1c11c5e5e2c067b3f52e9d0f12820fd02c2
John Doe <test-user@crowdbotics.com>
2023-12-14 15:57:25 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "mute-moon-44395",
-  url: "https://mute-moon-44395.botics.co",
-  api: "https://mute-moon-44395.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "mute-moon-44395",
+  "url": "https://mute-moon-44395.botics.co",
+  "api": "https://mute-moon-44395.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Cats"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
